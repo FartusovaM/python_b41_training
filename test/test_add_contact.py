6eec6a1227d66e8f6973a3baf4c64c967df63ea8
mariya.fartusova <mariya.fartusova@sbermarket.ru>
2023-11-19 12:54:10 +0400

    add new class for session management

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(usermane="admin", password="secret")
+    app.session.login(usermane="admin", password="secret")
     app.create_contact(Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test", address="test", home="test", mobile="test", work="test", fax="test", email="test", email2="test", email3="test", homepage="test", byear="1900", ayear="2000", address2="test", phone2="test", notes="test", bday="1", bmonth="February", amonth="February", aday="1"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(usermane="admin", password="secret")
+    app.session.login(usermane="admin", password="secret")
     app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", ayear="", address2="", phone2="", notes="", bday="1", bmonth="February", amonth="February", aday="1"))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(usermane="admin", password="secret")
+    app.session.login(usermane="admin", password="secret")
     app.create_group(Group(name="test", header="test", footer="test"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
