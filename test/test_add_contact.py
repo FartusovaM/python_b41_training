4a255817ef791edef52c748e4938c3491d35b370
mariya.fartusova <mariya.fartusova@sbermarket.ru>
2023-11-19 13:12:41 +0400

    added helper classes for contacts and groups (homework 5)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(usermane="admin", password="secret")
-    app.create_contact(Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test", address="test", home="test", mobile="test", work="test", fax="test", email="test", email2="test", email3="test", homepage="test", byear="1900", ayear="2000", address2="test", phone2="test", notes="test", bday="1", bmonth="February", amonth="February", aday="1"))
+    app.contact.create(Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test", address="test", home="test", mobile="test", work="test", fax="test", email="test", email2="test", email3="test", homepage="test", byear="1900", ayear="2000", address2="test", phone2="test", notes="test", bday="1", bmonth="February", amonth="February", aday="1"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(usermane="admin", password="secret")
-    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", ayear="", address2="", phone2="", notes="", bday="1", bmonth="February", amonth="February", aday="1"))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", ayear="", address2="", phone2="", notes="", bday="1", bmonth="February", amonth="February", aday="1"))
     app.session.logout()
